763e51a4b5ed9623341605f9bec49c3b89686384
Ruslan <alf.adman@gmail.com>
2022-01-06 16:54:58 +0200

    add files

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\UpdateItemRequest;
@@ -14,33 +15,34 @@ class ItemController extends Controller
     public function generateItems(Request $request)
     {
 
-
+        
         $userId = $request->user_id;
-        $items = DB::table('items')->select('*')->where('user_id', $userId)->first();
+        
+        $user = User::where('id', $userId)->exists();
+        $itemUser = Item::where('user_id', $userId )->doesntExist();
+        //$user = DB::table('users')->select('id')->where('id', $userId)->first();
+        //$items = DB::table('items')->select('*')->where('user_id', $userId)->first();
+       
+        if ($user && $itemUser) {
+                $num = rand(3, 20);
+                $water = ($num == 2) ? 2 + (rand(1, 18)) : $num;
 
-        if (!isset($items->user_id)) {
-
-            $num = rand(3, 20);
-            $water = ($num == 2) ? 2 + (rand(1, 18)) : $num;
-
-            $items = Item::create([
-                'user_id' => $userId,
-                'water' => $water,
-                'shirt' => rand(3, 20),
-                'pants' => rand(3, 20),
-                'dog' => rand(3, 20),
-                'soup' => rand(3, 20),
-                'be_developer' => rand(3, 20),
-            ]);
+                $items = Item::create([
+                    'user_id' => $userId,
+                    'water' => $water,
+                    'shirt' => rand(3, 20),
+                    'pants' => rand(3, 20),
+                    'dog' => rand(3, 20),
+                    'soup' => rand(3, 20),
+                    'be_developer' => rand(3, 20),
+                ]);
             return ResponseServise::sendJsonResponse(true, 200, [], [
                 'items' => $items->toArray()
             ]);
         } else {
             return ResponseServise::sendJsonResponse(false, 400, [], [
-                'message' => "User can't generate another set of items"
+                'message' => "User a not exist. Or User can't generate additional set of items"
             ]);
         }
     }
-
-
 }
---
+++ database/migrations/2022_01_06_132906_create_trades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            
+            $table->double('water');
+            $table->double('shirt');
+            $table->double('pants');
+            $table->double('dog');
+            $table->double('soup');
+            $table->double('be_developer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trades');
+    }
+}
